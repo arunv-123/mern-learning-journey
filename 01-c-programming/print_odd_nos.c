d4fead4ebb1f5b85c2a59d211ee57a1e72d84b68
Arun V <arunveekenchery@gmail.com>
2025-08-23 21:00:00 +0530

    Practice: print odd and even numbers

---
+++ 01-c-programming/print_odd_nos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+        /*for (int i=1; i<50; i+=2){
+        printf("%d \n",i);
+        }*/
+      int num;
+
+    for (num = 1; num <= 50; num++) {
+        if (num % 2 != 0) {
+            printf("%d\n", num);
+        }
+    }
+
+}
