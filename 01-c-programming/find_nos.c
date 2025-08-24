5c6f2c972b68ad2f694f501e15f34cc620005c30
Arun V <arunveekenchery@gmail.com>
2025-08-24 23:15:59 +0530

    Practice: Check if the given number is between 1 and 100

---
+++ 01-c-programming/find_nos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+        
+    printf("Enter a number:");
+    scanf("%d", &x);
+
+    if(x>1 && x<100){
+        printf("%d is between 1 and 100", x);
+    }
+    else{
+        printf("%d isnot between 1 and 100", x);
+    }
+}
