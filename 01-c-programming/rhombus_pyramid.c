8d95fb6d2019533827c864c8d0f16a1ed7bd0575
Arun V <arunveekenchery@gmail.com>
2025-08-27 11:36:40 +0530

    Practice: Inverted rhombus pattern printing

---
+++ 01-c-programming/rhombus_pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int num, i, j, k;
+    
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    for(i=0; i<num; i++){
+       for(j=0; j<i; j++){
+        printf(" ");
+       }
+       for(k=0; k<num; k++){
+        printf("*");
+       }
+       printf("\n");
+    }
+    
+}
