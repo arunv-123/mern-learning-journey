3fed933b3ae6a16eefd19bca0e691a6af86f8a09
Arun V <arunveekenchery@gmail.com>
2025-08-28 19:40:02 +0530

    Practice: Pattern printing - Hollow hour glass

---
+++ 01-c-programming/hollow_hour_glass.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+    int main(){
+        int num=5,i,j,k;
+
+        // printf("enter the number");
+        // scanf("%d",&num);
+
+    for (i=1;i<=num;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        if (i==1)
+        {
+             for (j=1;j<=num-1;j++)
+        {
+            printf(" *");
+        }
+        }
+        
+        else
+        {
+        
+        for(k=1;k<=2*(num-i)-1;k++)
+        {
+            printf(" ");
+        }
+        if (i!=num)
+        {
+            printf("*");
+        }
+    }
+        
+        printf("\n");
+    }
+
+        for (i=num-1;i>=1;i--)
+        {
+            for(j=1;j<=i;j++){
+
+            printf(" ");
+        
+        }
+        printf("*");
+          if (i==1)
+        {
+             for (j=1;j<=num-1;j++)
+        {
+            printf(" *");
+        }
+    }
+       else
+       {
+        
+    for(k=1;k<=2*(num-i)-1;k++)
+    {
+        printf(" ");
+    }
+    if (i!=num)
+    {
+    printf("*");
+    }
+
+    printf("\n");
+
+        
+    }
+        
+    }
+}
