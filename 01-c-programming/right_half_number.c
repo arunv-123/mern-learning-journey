e32603736a2258306bfdc2c831032facc97181bb
Arun V <arunveekenchery@gmail.com>
2025-08-28 21:18:20 +0530

    Practice: Pattern printing - Right half number

---
+++ 01-c-programming/right_half_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num=5,i,j;
+
+
+    for (i=1;i<=num;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            printf(" %d",i);
+        }
+        printf("\n");
+    }
+    
+}
