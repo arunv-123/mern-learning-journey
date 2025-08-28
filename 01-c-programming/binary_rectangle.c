387d7c02123a11ba9dda27d3c3e7b496990104b7
Arun V <arunveekenchery@gmail.com>
2025-08-28 22:27:35 +0530

    Fix: binary rectangle

--- 01-c-programming/binary_rectangle.c
+++ 01-c-programming/binary_rectangle.c
@@ -2,9 +2,6 @@
 int main(){
     int num,i,j,row=4,col=9;
 
-    printf("Enter a number:");
-    scanf("%d",&num);
-
     for(i=1;i<=row;i++){
         for(j=1;j<=col;j++){
             if(i==1 || i==row || j==1 || j==col ){
