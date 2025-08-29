8d842e818fb5b6134188628b5ae493727813365d
Arun V <arunveekenchery@gmail.com>
2025-08-29 20:22:55 +0530

    Practice: Pattern printing - Sequential Number Grid Pattern

---
+++ 01-c-programming/seq_no_grid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int i,j,num=5,a=1;
+
+    for(i=1;i<=num;i++){
+        for(j=1; j<=num;j++){
+            if(a<10){
+                printf("%d  ",a);
+            }
+            else{
+                printf("%d ",a);
+            }
+                a++;
+            }
+        
+        
+        printf("\n");
+    }
+}
