6eaa0701dfca9fcdfa4c51af395e1aff6a14ae0b
Arun V <arunveekenchery@gmail.com>
2025-08-29 20:25:51 +0530

    Practice: Pattern printing - Hollow Inverted Right-Angled Triangle

---
+++ 01-c-programming/hollow_inverted_right.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i,j,num=5;
+
+    for(i=1;i<=num;i++){
+        for(j=1; j<=num;j++){
+            if(j==1|| i==1|| j==num+1-i){
+                printf(" *");
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
