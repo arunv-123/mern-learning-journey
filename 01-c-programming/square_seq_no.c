f26529bc051fd4bca742f37dad389189c1c27b27
Arun V <arunveekenchery@gmail.com>
2025-08-29 20:28:50 +0530

    Practice: Pattern printing - Square Sequential Number Pattern

---
+++ 01-c-programming/square_seq_no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,j,num=5;
+
+    for(i=1;i<=num;i++){
+        for(j=1; j<=num;j++){
+                printf("%d",j);
+            }
+        
+        
+        printf("\n");
+    }
+}
