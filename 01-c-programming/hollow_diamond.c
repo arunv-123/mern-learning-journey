0f931db473e21741fd1041848fe2e58af099b2a8
Arun V <arunveekenchery@gmail.com>
2025-08-31 23:50:13 +0530

    Practice: Pattern printing - Solving hollow diamond

---
+++ 01-c-programming/hollow_diamond.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5, i, j, k;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j =  i; j < n; j++)
+        {
+            printf(" ");
+        }
+            printf("*");
+        
+        
+        for (k = 1; k <= 2*(i-1)-1/*important*/; k ++)
+        {
+            printf(" ");
+        }
+
+        if(i != 1)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        
+        
+    }
+    //bottom half
+    for (i = 2; i < n; i++)
+    {
+        for (j = 1; j < i; j++ )
+        {
+            printf(" ");
+        }
+        printf("*");
+
+        for (k = 1; k <= 2*((n-1)-i)-1; k++)
+        {
+            printf(" ");
+        }
+        
+        if(i < n)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+}
