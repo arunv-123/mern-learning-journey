3cbfbf0541907c54d91b569f917274a135214152
Arun V <arunveekenchery@gmail.com>
2025-11-11 12:00:00 +0530

    Practiced JavaScript basics

---
+++ 04-javascript/concepts/var_decl_keywords.js
@@ -0,0 +1,18 @@
+console.log("hii");
+var a = 10
+var a = "changeable when re-intitialized and reassigned"
+console.log(a)
+let b = 10
+//let b = "error" 
+b = "changeable only when reassigned"
+console.log(b)
+const c = 10
+//const c = "error"
+//c = "error"
+console.log("Non changeable if re-initialized or reassigned")
+
+
+
+//for loop -print 1 to 20
+//sum to 1 to 10
+// even numbers of 1 to 20
