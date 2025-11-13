39aaa9a4eea73ceea55c9632006ff8e877d4a5fb
Arun V <arunveekenchery@gmail.com>
2025-11-13 15:26:59 +0530

    Practiced array problem

---
+++ 04-javascript/concepts/array_prblm1.js
@@ -0,0 +1,6 @@
+let students = [{name:"arun",age:21,place:"ucity"},{name:"akshai",age:21,place:"nilambur"},{name:"adarsh",age:21,place:"chemmad"}]
+
+for (let i = 0; i < 3; i++)
+{
+    console.log(students[i].name,students[i].age);
+}
