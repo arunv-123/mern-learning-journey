a3596aaf0a94f08a2b4acefdbb9df9ee5cfb777f
Arun V <arunveekenchery@gmail.com>
2025-11-13 16:38:57 +0530

    Program to find whether a number is prime or not

---
+++ 04-javascript/concepts/prime_or_not.js
@@ -0,0 +1,39 @@
+for ( i = 2; i < 11; i++)
+{
+    if ( 11 % i == 0)
+    {
+        console.log("11 is not a prime number");
+        break
+    }
+    else if (i != 10)
+    {
+        continue
+    }
+    else
+    {
+        console.log("11 is a prime number");
+        
+    }
+}
+
+// let prime= true
+// for ( i = 2; i < 11; i++)
+// {
+//     if ( 11 % i == 0)
+//     {
+//         prime = false
+        
+//     }
+   
+// }
+
+// if (prime == false)
+// {
+//     console.log("11 is not a prime number");
+    
+// }
+// else
+// {
+//     console.log("11 is a prime number");
+    
+// }
