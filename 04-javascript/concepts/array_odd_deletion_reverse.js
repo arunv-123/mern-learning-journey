f578eb2e5f98263b55cb584c0d1d287246039bb8
Arun V <arunveekenchery@gmail.com>
2025-12-03 11:13:07 +0530

    Print only the even numbers in an array and reverse it

--- 04-javascript/concepts/array_odd_deletion_reverse.js
+++ 04-javascript/concepts/array_odd_deletion_reverse.js
@@ -1,7 +1,7 @@
 let array = [1,2,3,4,5,6,7,8,9,10]
 for ( let i = 0; i <= array.length-1; i++)
 {
-    if (array[i] % 2 == 0)
+    if (array[i] % 2 != 0)
     {
         for (let j = i; j <= array.length-1; j++)
         {
