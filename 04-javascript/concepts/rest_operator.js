c794deef019b6188f16b64b3bf5fcd814bab8639
Arun V <arunveekenchery@gmail.com>
2025-12-05 16:49:40 +0530

    Rest operator in js

---
+++ 04-javascript/concepts/rest_operator.js
@@ -0,0 +1,14 @@
+//shadow copy
+let arr = [1,2,3,4,5]
+let res = [...arr]
+let [a,b,...rest] = arr
+console.log(a,b);
+console.log(rest);
+
+
+function Test(x,y,...rest)
+{
+    console.log(x,y,...rest);
+    
+}
+Test(...arr)
