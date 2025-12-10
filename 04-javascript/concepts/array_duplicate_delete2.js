92e11d4dca49d78251ed08c346a85766b2ac4194
Arun V <arunveekenchery@gmail.com>
2025-12-10 23:09:25 +0530

    Cleared a mistake

--- 04-javascript/concepts/array_duplicate_delete2.js
+++ 04-javascript/concepts/array_duplicate_delete2.js
@@ -1,27 +1,63 @@
-arr = [1,4,5,7,4,5,11]
-for (let i = 0; i < arr.length; i++)
-{
-    for (let j = 0; j < arr.length; j++)
-    {
-        if (i == j)
-        {
-            continue
+// arr = [1,4,5,7,4,5,11]
+// for (let i = 0; i < arr.length; i++)
+// {
+//     for (let j = 0; j < arr.length; j++)
+//     {
+//         if (i == j)
+//         {
+//             continue
+//         }
+//         else if (arr[i] == arr[j])
+//         {
+//             for (let k = i; k < arr.length; k++)
+//             {
+//                 arr[k] = arr[k+1]
+//             }
+//             arr.length--
+//             j--
+//             for (let h = j; h < arr.length; h++)
+//             {
+//                 arr[h] = arr[h+1]
+//             }
+//             arr.length--
+//             i--
+//         }
+//     }
+// }
+// console.log(arr);
+
+
+let arr =[1,4,5,7,4,5,11]  
+let count =0;
+for (let i =0; i<arr.length;i++) {
+
+
+
+    for( let j =0; j<arr.length;j++){
+        if(i==j){
+          continue
+         
         }
-        else if (arr[i] == arr[j])
-        {
-            for (let k = i; k < arr.length; k++)
-            {
-                arr[k] = arr[k+1]
+        else if(arr[i]==arr[j]){
+            
+          for (let k = j; k < arr.length; k++) {
+               
+            arr[k] = arr[k + 1];
+           
             }
-            arr.length--
-            j--
-            for (let h = j; h < arr.length; h++)
-            {
-                arr[h] = arr[h+1]
-            }
-            arr.length--
-            i--
-        }
-    }
+
+            arr.length--;
+
+            for( l= i;l<arr.length;l++){
+   
+            arr[l]=arr[l+1];
+           
+          }
+
+            arr.length--;
+            i--;
+  }        
+}
+
 }
 console.log(arr);
