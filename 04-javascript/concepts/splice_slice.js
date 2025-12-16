2f6d663acf631619cf76f7d3ee8c1e449123a167
Arun V <arunveekenchery@gmail.com>
2025-12-16 15:35:18 +0530

    Splice and Slice in js

---
+++ 04-javascript/concepts/splice_slice.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,6,7,8,9]
+res = arr.slice(2,5)//(strarting point, ending point)
+console.log(res);
+
+spli = arr.splice(3,2)//(starting point,count) splice will delte the cutted index from the whole array
+console.log(spli);
