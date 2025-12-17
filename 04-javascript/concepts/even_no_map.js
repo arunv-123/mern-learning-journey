f4ca11802e93ca61a3b001601c5e9c48744713eb
Arun V <arunveekenchery@gmail.com>
2025-12-17 22:28:03 +0530

    Map method in js

---
+++ 04-javascript/concepts/even_no_map.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,6,7,8,9,10]
+arr.map((value)=>{
+    if(value % 2 == 0)
+    {
+        console.log(value);
+        
+    }
+})
---
+++ 04-javascript/concepts/map_method.js
@@ -0,0 +1,7 @@
+let arr = [10,20,30,40,50]
+
+arr.map((value,index,full)=>{
+    console.log(value);
+    console.log(index);
+    console.log(full);   
+})
